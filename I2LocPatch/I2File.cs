5dfdccef232108f74b3bf9edee22ce78f96f3a25
宵夜 <34893504+xiaoye97@users.noreply.github.com>
2022-07-18 20:15:29 +0800

    上传

---
+++ I2LocPatch/I2File.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace I2LocPatch
+{
+    public class I2File
+    {
+        public static char newLineChar = 'þ';
+        // טּ
+        public static char sep = '\t';
+        public string Name;
+        public List<string> Languages = new List<string>();
+        public List<TermLine> Lines = new List<TermLine>();
+
+        /// <summary>
+        /// 将表格写入文件
+        /// </summary>
+        /// <param name="path"></param>
+        public void WriteCSVTable(string path)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            // 写入表头
+            sb.Append($"Key{sep}");
+            int langLen = Languages.Count;
+            for (int i = 0; i < langLen; i++)
+            {
+                sb.Append(Languages[i]);
+                if (i < langLen-1)
+                {
+                    sb.Append(sep);
+                }
+                else
+                {
+                    sb.Append('\n');
+                }
+            }
+            // 写入数据
+            foreach (var line in Lines)
+            {
+                string key = line.Name.Replace('\n', newLineChar);
+                sb.Append($"{key}{sep}");
+                for (int i = 0; i < langLen; i++)
+                {
+                    string text = line.Texts[i].Replace('\r', newLineChar).Replace('\n', newLineChar);
+                    sb.Append(text);
+                    if (i < langLen - 1)
+                    {
+                        sb.Append(sep);
+                    }
+                    else
+                    {
+                        sb.Append("\n");
+                    }
+                }
+            }
+            try
+            {
+                FileInfo fileInfo = new FileInfo(path);
+                if (!fileInfo.Directory.Exists)
+                {
+                    fileInfo.Directory.Create();
+                }
+                File.WriteAllText(path, sb.ToString());
+            }
+            catch (Exception ex)
+            {
+                I2LocPatchPlugin.LogError("写入文件出现异常");
+                I2LocPatchPlugin.LogError(ex.ToString());
+            }
+        }
+
+        /// <summary>
+        /// 从CSV文件加载语言(单语言模式)
+        /// </summary>
+        /// <param name="path"></param>
+        public static I2File LoadFromCSVSingleMode(string path)
+        {
+            try
+            {
+                char loadSep = I2LocPatchPlugin.Instance.CommaSepWhenLoad.Value ? ',' : sep;
+                FileInfo fileInfo = new FileInfo(path);
+                if (fileInfo.Exists)
+                {
+                    var lines = File.ReadAllLines(path);
+                    I2File i2File = new I2File();
+                    i2File.Name = fileInfo.Name;
+                    i2File.Languages.Add(i2File.Name);
+                    I2LocPatchPlugin.LogInfo($"开始读取语言文件：{i2File.Name}");
+                    if (lines.Length < 2)
+                    {
+                        I2LocPatchPlugin.LogError($"文件行数小于2行");
+                        return null;
+                    }
+                    for (int i = 1; i < lines.Length; i++)
+                    {
+                        if (string.IsNullOrWhiteSpace(lines[i])) continue;
+                        var kv = lines[i].Split(new char[] { loadSep }, 2);
+                        if (kv.Length != 2) continue;
+                        if (I2LocPatchPlugin.Instance.DevMode.Value)
+                        {
+                            I2LocPatchPlugin.LogInfo($"index:{i} {kv[0]} {kv[1]}");
+                        }
+                        if (string.IsNullOrWhiteSpace(kv[1]))
+                        {
+                            // 没有内容，跳过
+                            continue;
+                        }
+                        TermLine line = new TermLine();
+                        line.Name = kv[0];
+                        line.Texts = new string[1];
+                        line.Texts[0] = kv[1].Replace(newLineChar, '\n');
+                        i2File.Lines.Add(line);
+                    }
+                    I2LocPatchPlugin.LogInfo($"{path} 读取完毕，共{i2File.Lines.Count}条翻译");
+                    return i2File;
+                }
+                else
+                {
+                    I2LocPatchPlugin.LogError($"要读取的文件不存在 path:{path}");
+                }
+            }
+            catch (Exception ex)
+            {
+                I2LocPatchPlugin.LogError($"读取文件出现异常 path:{path}");
+                I2LocPatchPlugin.LogError(ex.ToString());
+            }
+            return null;
+        }
+    }
+
+    public class TermLine
+    {
+        public string Name;
+        public string[] Texts;
+    }
+}
---
+++ I2LocPatch/I2LocPatchPlugin.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using HarmonyLib;
+using I2.Loc;
+using UnityEngine;
+using System.IO;
+using BepInEx.Configuration;
+
+namespace I2LocPatch
+{
+    [BepInPlugin("xiaoye97.I2LocPatch", "I2LocPatch", "1.0.0")]
+    public class I2LocPatchPlugin : BaseUnityPlugin
+    {
+        public static I2LocPatchPlugin Instance;
+        public ConfigEntry<string> TargetLanguage;
+        public ConfigEntry<bool> DevMode;
+        public ConfigEntry<bool> CommaSepWhenLoad;
+
+        void Awake()
+        {
+            Instance = this;
+            DevMode = Config.Bind<bool>("Dev", "DevMode", false, "开发模式时，按下Ctrl+Keypad1进行Dump文本");
+            TargetLanguage = Config.Bind<string>("config", "TargetLanguage", "Chinese", "目标语言");
+            CommaSepWhenLoad = Config.Bind<bool>("config", "CommaSepWhenLoad", true, "在加载csv时使用逗号分隔而不是制表符");
+        }
+
+        void Start()
+        {
+            Invoke("LoadCsv", 1f);
+        }
+
+        public void LoadCsv()
+        {
+            var i2Files = LoadAllI2CsvSingleMode();
+            if (i2Files != null && i2Files.Count > 0)
+            {
+                var mResourcesCache = Traverse.Create(ResourceManager.pInstance).Field("mResourcesCache").GetValue<Dictionary<string, UnityEngine.Object>>();
+                if (mResourcesCache != null)
+                {
+                    System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+                    sw.Start();
+                    foreach (var kv in mResourcesCache)
+                    {
+                        // 语言资源
+                        if (kv.Value != null && kv.Value is LanguageSourceAsset)
+                        {
+                            var asset = kv.Value as LanguageSourceAsset;
+                            if (asset.SourceData.GetLanguages().Contains(TargetLanguage.Value))
+                            {
+                                int langCount = asset.SourceData.GetLanguages().Count;
+                                int index = asset.SourceData.GetLanguageIndex(TargetLanguage.Value);
+                                foreach (var i2File in i2Files)
+                                {
+                                    foreach (var line in i2File.Lines)
+                                    {
+                                        if (asset.SourceData.ContainsTerm(line.Name))
+                                        {
+                                            var term = asset.SourceData.GetTermData(line.Name);
+                                            term.SetTranslation(index, line.Texts[0]);
+                                        }
+                                        else
+                                        {
+                                            TermData term = new TermData();
+                                            term.TermType = eTermType.Text;
+                                            List<string> languages = new List<string>();
+                                            for (int i = 0; i < langCount; i++)
+                                            {
+                                                languages.Add(line.Texts[i]);
+                                            }
+                                            term.Languages = languages.ToArray();
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    LocalizationManager.CurrentLanguage = "Chinese";
+                    sw.Stop();
+                    LogInfo($"LoadCsv加载完毕 耗时{sw.ElapsedMilliseconds}ms");
+                }
+            }
+        }
+
+        public static void LogInfo(string log)
+        {
+            Instance.Logger.LogInfo(log);
+        }
+
+        public static void LogError(string log)
+        {
+            Instance.Logger.LogError(log);
+        }
+
+        void Update()
+        {
+            if (DevMode.Value)
+            {
+                if (Input.GetKey(KeyCode.LeftControl) && Input.GetKeyDown(KeyCode.Keypad1))
+                {
+                    DumpAllLocRes();
+                }
+            }
+        }
+
+        public void DumpAllLocRes()
+        {
+            var mResourcesCache = Traverse.Create(ResourceManager.pInstance).Field("mResourcesCache").GetValue<Dictionary<string, UnityEngine.Object>>();
+            if (mResourcesCache != null)
+            {
+                foreach (var kv in mResourcesCache)
+                {
+                    // 语言资源
+                    if (kv.Value != null && kv.Value is LanguageSourceAsset)
+                    {
+                        DumpLocRes(kv.Value as LanguageSourceAsset);
+                    }
+                }
+            }
+            LogInfo($"全部Dump完毕");
+        }
+
+        public void DumpLocRes(LanguageSourceAsset asset)
+        {
+            I2File i2File = new I2File();
+            i2File.Name = asset.name;
+            i2File.Languages = asset.SourceData.GetLanguages();
+            //int langLen = i2File.Languages.Count;
+            int termLen = asset.SourceData.mTerms.Count;
+            for (int i = 0; i < termLen; i++)
+            {
+                var term = asset.SourceData.mTerms[i];
+                if (term.TermType == eTermType.Text)
+                {
+                    if (string.IsNullOrWhiteSpace(term.Term))
+                    {
+                        continue;
+                    }
+                    TermLine line = new TermLine();
+                    line.Name = term.Term;
+                    line.Texts = term.Languages;
+                    i2File.Lines.Add(line);
+                }
+            }
+            //i2File.WriteTest($"{Paths.GameRootPath}/I2/Test.txt");
+            i2File.WriteCSVTable($"{Paths.GameRootPath}/I2/{i2File.Name}.csv");
+            LogInfo($"Dump {i2File.Name}完毕");
+        }
+
+        /// <summary>
+        /// 加载所有的翻译后的CSV，搜索plugins/I2LocPatch文件夹
+        /// </summary>
+        public List<I2File> LoadAllI2CsvSingleMode()
+        {
+            try
+            {
+                DirectoryInfo dir = new DirectoryInfo($"{Paths.PluginPath}/I2LocPatch");
+                if (!dir.Exists)
+                {
+                    dir.Create();
+                }
+                var files = dir.GetFiles("*.csv");
+                List<I2File> i2Files = new List<I2File>();
+                for (int i = 0; i < files.Length; i++)
+                {
+                    I2File file = I2File.LoadFromCSVSingleMode(files[i].FullName);
+                    if (file != null)
+                    {
+                        i2Files.Add(file);
+                    }
+                }
+                return i2Files;
+            }
+            catch (Exception ex)
+            {
+                LogError(ex.ToString());
+            }
+            return null;
+        }
+    }
+}
